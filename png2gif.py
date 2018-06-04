6b2118ea4939a1e33825799fa6ff4ff869c7baba
oscar <objones@calpoly.edu>
2018-06-03 19:02:34 -0700

    added ability to easily configure level of abstraction present in dreams

--- png2gif.py
+++ png2gif.py
@@ -12,23 +12,70 @@ BASE_SRC = "./imsrc/"
 BASE_OUT = "./imout/"
 fetch_loss_and_grads = None
 
-def set_contributions(TODO_some_default_params=0): #TODO make more easily configurable
-    return {'mixed2': 0.2,
-            'mixed3': 3.,
-            'mixed4': 4.,
-            'mixed5': 1.5,
-            }
-    
-def main():
+def set_contributions(arch_dict, depth_tgt=0.5):
+    # depth_tgt is the targeted level of abstraction used to determine which
+    # layers influence the final loss function most. layers nearer to the end
+    # of the architecture should produce representations with higher levels of
+    # abstraction, for example, an animal eye is a higher layer of abstraction
+    # than a geometric pattern- layers nearer to the beginning of the
+    # architecture will produce more geometric-pattern-like representations.
+    #
+    # depth_tgt of 0 will result in a look closer to the geometric-pattern end
+    # of the spectrum, while a depth_tgt of 1 will result in a look in which
+    # higher levels of abstraction should become visible.
+    #
+    # chollet_base = {'mixed2': 0.2, 'mixed3': 3., 'mixed4': 4., 'mixed5': 1.5}
+
+    # list of chosen layers to be used in loss fn, sorted
+    # in order of appearance in the architecture (or close to it)
+    candidates = []
+
+    filter_str = "mixed"
+    for lyrnm, lyr in arch_dict.items():
+        if (filter_str in lyrnm):
+            non_filter_part = lyrnm[len(filter_str):]
+
+            try:
+                srtV = int(non_filter_part)
+            except ValueError:
+                srtV = int(non_filter_part[:non_filter_part.find('_')])
+
+            candidates.append((srtV, lyrnm))
+
+    tgt_num_lyrs_that_influence_loss = 5
+
+    candidates.sort()
+    cand_depths = np.linspace(0, 1, len(candidates))
+    err = abs(cand_depths - depth_tgt)
+    coeffs = -np.log(err + 0.01)
+    prob = coeffs / np.sum(coeffs)  # adjust prob so its a distr that sums to 1
+    selections = np.random.multinomial(tgt_num_lyrs_that_influence_loss, prob)
+
+    rtn = {}
+    for i, result in enumerate(selections):
+        if (result):
+            rtn[candidates[i][1]] = result * coeffs[i]
+
+    return rtn
+
+
+def set_FLGRAD(loss, dream):
     global fetch_loss_and_grads
+
+    grads = K.gradients(loss, dream)[0]
+    grads /= K.maximum(K.mean(K.abs(grads)), 1e-7)  # normalize
+    outputs = [loss, grads]
+
+    fetch_loss_and_grads = K.function([dream], outputs)
+
+
+def main():
     check_args()
     K.set_learning_phase(0)  # disables all training specific operations
     model = inception_v3.InceptionV3(weights='imagenet', include_top=False)
-    print(model.summary())
-    return
     layer_dict = dict([(layer.name, layer) for layer in model.layers])
 
-    layer_contributions = set_contributions()
+    layer_contributions = set_contributions(layer_dict, depth_tgt=0.99)
 
     loss = K.variable(0.)
     for layer_name in layer_contributions:
@@ -36,22 +83,20 @@ def main():
         coeff = layer_contributions[layer_name]
         activation = layer_dict[layer_name].output
         scaling = K.prod(K.cast(K.shape(activation), 'float32'))
-        th = coeff * K.sum(K.square(activation[:, 2: -2, 2: -2, :])) / scaling
-        loss += th
+        lyrloss = (coeff * K.sum(K.square(activation[:, 2: -2, 2: -2, :]))
+                   / scaling)
+        loss = loss + lyrloss
 
-    dream = model.input
-    grads = K.gradients(loss, dream)[0]
-    grads /= K.maximum(K.mean(K.abs(grads)), 1e-7)  # normalize
-    outputs = [loss, grads]
-    fetch_loss_and_grads = K.function([dream], outputs)
+    set_FLGRAD(loss, model.input)
+
 
 # begin deep dream algorithm
-    #hyperparams
+    # hyperparams
     step = 0.01
     nframes = int(sys.argv[2])
     max_loss = None
 
-    #set up path
+    # set up path
     img_name = sys.argv[1]
     path = BASE_SRC + img_name
     img_base_name = img_name[0:img_name.find('.')]
@@ -83,6 +128,7 @@ def eval_loss_and_grads(x):
     grad_values = outs[1]
     return loss_value, grad_values
 
+
 def gradient_ascent(x, iterations, step, max_loss=None):
     for i in range(iterations):
         loss_value, grad_values = eval_loss_and_grads(x)
@@ -90,6 +136,7 @@ def gradient_ascent(x, iterations, step, max_loss=None):
             break
         x += step * grad_values
     return x
+
 
 # img processing functions
 def save_gif(frame_list, fname):
@@ -104,9 +151,11 @@ def save_gif(frame_list, fname):
         images.append(imageio.imread(BASE_OUT + filename))
     imageio.mimsave(BASE_OUT + fname, images, duration=0.1)
 
+
 def save_img(img, fname):
     pil_img = deprocess_image(np.copy(img))
     scipy.misc.imsave(BASE_OUT + fname, pil_img)
+
 
 def preprocess_image(image_path):
     img = image.load_img(image_path)
@@ -114,6 +163,7 @@ def preprocess_image(image_path):
     img = np.expand_dims(img, axis=0)
     img = inception_v3.preprocess_input(img)
     return img
+
 
 def deprocess_image(x):
     if K.image_data_format() == 'channels_first':
@@ -126,6 +176,7 @@ def deprocess_image(x):
     x *= 255.
     x = np.clip(x, 0, 255).astype('uint8')
     return x
+
 
 # ----------------------- run main ------------------------------
 if __name__ == "__main__":

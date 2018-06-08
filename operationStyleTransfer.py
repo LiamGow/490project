313770ae7b53d4508d3883d207899598df6a7b31
Liam Gow <liam.m.gow@gmail.com>
2018-06-08 01:20:35 -0700

    adding operationStyleTransfer

--- operationStyleTransfer.py
+++ operationStyleTransfer.py
@@ -84,7 +84,7 @@ class StyleTransfer(Operation):
         # get tensor representations of our images
         base_image = K.variable(img)
         style_reference_image = K.variable(
-            load_img(self.args.style, self.network.preprocess_func))
+            load_img(self.args.style, self.network.preprocess_image))
 
         # this will contain our generated image
         if K.image_data_format() == 'channels_first':

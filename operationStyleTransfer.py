70a1b1386f5dde161e8f31ebb97524bef641a585
Liam Gow <liam.m.gow@gmail.com>
2018-06-08 02:39:03 -0700

    adding operationStyleTransfer

--- operationStyleTransfer.py
+++ operationStyleTransfer.py
@@ -86,7 +86,7 @@ class StyleTransfer(Operation):
 
         # base_image = K.variable(img)
         base_image = Image.fromarray(self.network.deprocess_image(img, width, height))
-        base_image = base_image.resize((img_nrows, img_ncols), Image.LANCZOS)
+        base_image = base_image.resize((img_ncols, img_nrows), Image.LANCZOS)
         # base_image = K.variable(self.network.preprocess_image(base_image))
         base_image = self.network.preprocess_image(base_image)
         print(base_image.shape)

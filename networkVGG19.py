cfdd3626804639269cf70c86a4331240578b359b
Liam Gow <liam.m.gow@gmail.com>
2018-06-08 01:43:58 -0700

    adding operationStyleTransfer

--- networkVGG19.py
+++ networkVGG19.py
@@ -21,7 +21,7 @@ class VGG19(Network):
     def preprocess_image(img):
         img = image.img_to_array(img)
         img = np.expand_dims(img, axis=0)
-        print(img.size)
+        print(img.shape)
         return vgg19.preprocess_input(img)
 
     # util function to convert a tensor into a valid image
